30aaba61f6a7129be6fd811a55fb4ccf1931d30b
rgpsico <rgyr2010@hotmail.com>
2018-10-03 06:40:22 -0300

    Curso css3 treinamento flex

---
+++ aula01.php
@@ -0,0 +1,30 @@
+<html>
+<link rel="stylesheet" href="style.css">
+	<body>
+		<section class="container">
+<article class="item article-01">
+	Article 01
+
+</article>
+<article class=" item article-02">
+	article 02
+</article>
+<article class=" item article-03">
+	article 03
+
+</article>
+
+
+
+
+<article class="item article-01">
+	Article 04
+
+</article>
+
+
+
+		</section>
+	</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,130 @@
+<html>
+<head>
+
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+<meta charset="utf-8">
+<meta name="viewpoint" content="width=device-width, inicial-scale=1">
+<title>Hotel Paraiso</title>
+</head>
+	<body>
+	
+
+<header>
+	<a href="#">
+		
+<img src="img/logo.png" alt=""
+alt="Hotel paraiso">
+
+	</a>
+
+
+<nav>
+	<li><a href=""></a>Home</li>
+    <li><a href=""></a>Sobre</li>
+    <li><a href=""></a>Reserva</li>
+    <li><a href=""></a>Contato</li>
+</nav>
+</header>
+
+
+<section class="hero">
+	
+<h1>Conhecer Esse paraiso chamado Itacare</h1>
+
+<h3>"Aconchego , conforto e simpatia"</h3>
+<a href="#" class="btn">Saiba Mais</a>
+
+
+</section>
+
+
+<section class="conheca-o-hotel">
+	<h3>Conheça o paraiso!</h3>
+<p>Em design gráfico e editoração, Lorem ipsum é um texto utilizado para preencher o espaço de texto em publicações e testar aspectos visuais, com a finalidade de verificar o layout, tipografia e formatação antes de utilizar conteúdo rea 
+</p>
+<hr>
+<ul class="grid">
+	
+	<li class="small" 
+style="background-image:url('img/itacare-01.jpg');"></li>
+
+
+	<li class="large" 
+style="background-image:url('img/itacare-02.jpg');"></li>
+	<li class="large" 
+style="background-image:url('img/itacare-03.jpg');"></li>
+	<li class="small" 
+style="background-image:url('img/itacare-04.jpg');"></li>
+</ul>
+
+
+
+</section>
+
+
+
+<section class="caracteristicas">
+	<h3>Em design gráfico</h3>
+<p>Em design gráfico e editoração, Lorem ipsum é um texto utilizado para preencher o espaço de texto em publicações e testar aspectos visuais, com a finalidade de verificar o layout, tipografia e formatação antes de utilizar conteúdo rea</p>
+<hr>
+<ul class="grid">
+	<li><i class="fas fa-location-arrow"></i>
+	<h4>Localização</h4>
+	<p>Em design gráfico e editoração, Lorem ipsum é um texto utilizado para preencher o espaço de texto em publicações e testar aspectos visuais, com a finalidade de verificar o layout, tipografia e formatação antes de utilizar conteúdo rea</p>
+	</li>
+
+
+<li><i class="fas fa-gift"></i>
+	<h4>Ofertas</h4>
+	<p>Em design gráfico e editoração, Lorem ipsum é um texto utilizado para preencher o espaço de texto em publicações e testar aspectos visuais, com a finalidade de verificar o layout, tipografia e formatação antes de utilizar conteúdo rea</p>
+	</li>
+
+
+
+<li><i class="fas fa-home"></i>
+	<h4>Acomodações</h4>
+	<p>Em design gráfico e editoração, Lorem ipsum é um texto utilizado para preencher o espaço de texto em publicações e testar aspectos visuais, com a finalidade de verificar o layout, tipografia e formatação antes de utilizar conteúdo rea</p>
+	</li>
+
+
+</ul>
+
+</section>
+
+
+
+<section class="newsletter">
+	
+<h3>Newsletter</h3>
+<form>
+	
+<input type="text" placeholder="Email">
+<button>Cadastrar</button>
+
+</form>
+
+</section>
+
+
+
+<footer>
+	<ul>
+		
+	<li><a href=""><i class="fab fa-facebook"></i></a></li>
+	<li><a href=""><i class="fab fa-facebook"></i></a></li>
+	<li><a href=""><i class="fab fa-facebook"></i></a></li>
+	<li><a href=""><i class="fab fa-facebook"></i></a></li>
+	
+
+
+	</ul>
+<p>
+	Em design gráfico e editoração, Lorem ipsum é um texto utilizado para preencher o espaço de texto em publicações e testar aspectos visuais, com a finalidade de verificar o layout, tipografia e formatação antes de utilizar conteúdo rea
+</p>
+</footer>
+
+
+	</body>
+
+</html>
